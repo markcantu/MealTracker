ac5bb6a1545edd776db00a9d95e82c3541b96b6e
EC2 Default User <ec2-user@ip-172-31-20-17.us-east-2.compute.internal>
2019-04-19 14:37:15 +0000

    Generate and run Item migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_175443) do
+ActiveRecord::Schema.define(version: 2019_04_19_143437) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,12 +33,10 @@ ActiveRecord::Schema.define(version: 2019_04_18_175443) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
