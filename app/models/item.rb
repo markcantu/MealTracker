882377dabfddd53bf0cef5ed734e9745ee5d772b
Matthew McCoy <mmccoy23@uncc.edu>
2019-04-21 01:57:43 +0000

    Recreated a comments association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+    has_many :comments
+    
     validates :title,
     presence: true,
     length: { minimum: 3 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_005603) do
+ActiveRecord::Schema.define(version: 2019_04_21_015539) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2019_04_21_005603) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
