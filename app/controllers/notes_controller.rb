07892bee44d4d8a55324c963fd7147aa3561673e
Matthew McCoy <mmccoy23@uncc.edu>
2019-04-21 01:35:50 +0000

    Add notes controller

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,12 @@
+class NotesController < ApplicationController
+    def create
+        @item = Item.find(params[:item_id])
+        @note = @item.notes.create(note_params)
+        redirect_to item_path(@item)
+    end
+    
+    private
+        def note_params
+            params.require(:note).permit(:body)
+        end
+end
