70c1d38a816f933bfe6e9f22394b7d008c4bcfcb
Matthew McCoy <mmccoy23@uncc.edu>
2019-04-25 19:04:35 +0000

    Added partials

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
         redirect_to item_path(@item)
     end
     
+    def destroy
+        @item = Item.find(params[:item_id])
+        @comment = @item.comments.find(params[:id])
+        @comment.destroy
+        redirect_to item_path(@item)
+    end
+    
     private
         def comments_params
             params.require(:comment).permit(:commenter, :body)  
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-    has_many :comments, dependent: :delete_all
+    has_many :comments, dependent: :delete_all, dependent: :destroy
     has_one_attached :image1
     has_one_attached :image2
     has_one_attached :image3
