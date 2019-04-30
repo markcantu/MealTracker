fa5e43847e4511588fe8261e85a27ac7dfdeff8f
Matthew McCoy <mmccoy23@uncc.edu>
2019-04-30 00:51:07 +0000

    Finalized GOogle Sign in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :items do
