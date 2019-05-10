b4ea754578a99f3f72c2e762ab124120e7f01253
Robert <rbeckhamUNCC@uncc.edu>
2019-05-10 14:38:34 +0000

    tightened things up/removed validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,31 +3,4 @@ class Item < ApplicationRecord
     has_one_attached :image1
     has_one_attached :image2
     has_one_attached :image3
-    
-    validates :title1,
-    presence: true,
-    length: { minimum: 3 }
-    
-    validates :text1,
-    presence: true,
-    length: { minimum: 5 }
-    
-    validates :title2,
-    presence: true,
-    length: { minimum: 3 }
-    
-    validates :text2,
-    presence: true,
-    length: { minimum: 5 }
-    
-    validates :title3,
-    presence: true,
-    length: { minimum: 3 }
-    
-    validates :text3,
-    presence: true,
-    length: { minimum: 5 }
-    
-    validates :date,
-    presence: true
 end
